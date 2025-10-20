276ad75ad28da62f0d9fa564e7f9846f2fd13c74
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-10-20 16:17:33 +0600

    update base api

--- src/redux/feature/baseApi.ts
+++ src/redux/feature/baseApi.ts
@@ -4,8 +4,8 @@ import { SetUserProfile } from './profile/profileSlice';
 import { RootState } from '../store';
 
 const rawBaseQuery = fetchBaseQuery({
-    // baseUrl: 'https://paulina-e-learning-platform.vercel.app/api/v1',
-    baseUrl: 'http://10.10.20.26:6080/api/v1',
+    baseUrl: 'https://paulina-e-learning-platform.vercel.app/api/v1',
+    // baseUrl: 'http://10.10.20.26:6080/api/v1',
 
     prepareHeaders: (headers, { getState }) => {
         const state = getState() as RootState;
