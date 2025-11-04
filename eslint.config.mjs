081c09a594f7353e341a6ba57fe7fd433ffc5f9d
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-11-04 11:24:36 +0600

    add eslint ruls

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,27 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "no-unused-vars": ["error", { "args": "none" }],
+      "no-console": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+      "prefer-const": "warn",
+      "no-var": "error",
+      "eqeqeq": ["error", "always"],
+      "no-multiple-empty-lines": ["warn", { "max": 1, "maxEOF": 1 }],
+      "object-curly-spacing": ["warn", "always"],
+      "array-bracket-spacing": ["warn", "never"],
+      "comma-spacing": ["warn", { "before": false, "after": true }],
+      "semi": ["warn", "always"],
+      "quotes": ["warn", "double"],
+      "indent": ["warn", 2, { "SwitchCase": 1 }],
+    }
+  },
+  {
+
     ignores: [
       "node_modules/**",
       ".next/**",
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
-import { ThemeProvider } from "next-themes";
+// import { ThemeProvider } from "next-themes";
 import { ReduxProvider } from "@/components/provider/ReduxProvider";
 import "./globals.css";
 
@@ -23,19 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${poppins.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} antialiased`}>
         <ReduxProvider>
-          <ThemeProvider
+          {/* <ThemeProvider
             attribute="class"
             defaultTheme="light"
-            // enableSystem
-            // disableTransitionOnChange
-          >
-            {children}
-            <Toaster richColors/>
-          </ThemeProvider>
+            enableSystem
+            disableTransitionOnChange
+          > */}
+          {children}
+          <Toaster richColors />
+          {/* </ThemeProvider> */}
         </ReduxProvider>
       </body>
     </html>
